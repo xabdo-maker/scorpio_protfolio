cba3eb0e09fa2ab2efd833fa42ab26f57e2776b8
Abdo <xabdo8207@gmail.com>
2025-03-24 01:17:50 +0200

    great one section for my services and add some features

---
+++ js/main.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+    el: "#NET",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x240fd4,
+    backgroundColor: 0x11003b,
+    spacing: 14.00
+  })
