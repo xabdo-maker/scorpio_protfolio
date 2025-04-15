01e5dae7a114199494cb9f67c478ff0ee4d404aa
Abdo <xabdo8207@gmail.com>
2025-04-15 14:26:35 +0200

    update the responsive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,11 @@ VANTA.NET({
     backgroundColor: 0x11003b,
     spacing: 14.00
   })
+
+   /* JavaScript إضافة هذا الكود لملف */
+   document.querySelector('.services-toggle')?.addEventListener('click', function() {
+    document.querySelectorAll('.services-content').forEach(el => {
+      el.style.display = 'block';
+    });
+    this.style.display = 'none';
+  });
